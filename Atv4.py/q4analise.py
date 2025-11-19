1a5ce2c701c409fdb0e534a07b39f14a16adf9bb
MariaReis19 <mariahdanatividae@gmail.com>
2025-11-19 00:28:11 -0300

    atividades dia 17/11

--- Atv4.py/q4analise.py
+++ Atv4.py/q4analise.py
@@ -1,10 +1,4 @@
-"""Crie um programa que solicite ao usuário que insira números
-inteiros. O programa deve continuar solicitando números até
-que o usuário digite 'fim'. Para cada número inserido, o
-programa deve informar se é par ou ímpar. Se o usuário
-inserir algo que não seja um número inteiro, o programa deve
-informar o erro e continuar. No final, o programa deve exibir a
-quantidade de números pares e ímpares inseridos."""
+"""4 - Criar um código que serve para analisar números digitados pelo usuário, classificando-os como pares ou ímpares e contabilizando quantos de cada tipo foram inseridos."""
 
 
 # Função para verificar se um número é par ou ímpar
